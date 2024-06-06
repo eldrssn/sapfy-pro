f98fc632e5e66459d2bce4a946c61fa18e3ab6c2
maxkalimullin <maxkalimullin@gmail.com>
2024-06-07 00:19:48 +0500

    fix task

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Link from 'next/link';
 import getRandomTask from '@/utils/getRandomTask';
-import { tasksData } from '@/data/dataForProps';
+import { tasks } from '@/data/tasks';
 import style from './styles.module.scss';
 import '../../app/global.scss';
 
 function Tasks() {
-  const randomTask = getRandomTask(tasksData);
+  const randomTask = getRandomTask(tasks);
 
   return (
     <section className={style.modalContainer}>
@@ -15,7 +15,9 @@ function Tasks() {
         <p className={style.description}>{randomTask.taskDescription}</p>
       </div>
 
-      <Link href={'/'} className={style.closeBtn}>← закрыть</Link>
+      <Link href={'/'} className={style.closeBtn}>
+        ← закрыть
+      </Link>
     </section>
   );
 }
